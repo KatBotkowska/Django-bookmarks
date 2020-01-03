65697bfcd264b26441c2017a8281ba5e6a720fdb
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-01-03 20:39:07 +0100

    model user for creating account

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -11,11 +11,11 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
-            #authenticate() -check if username and password are correct and return user object
+            # authenticate() -check if username and password are correct and return user object
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    #put user in current session
+                    # put user in current session
                     return HttpResponse('Authentication with success.')
                 else:
                     return HttpResponse('Account not active')
@@ -23,9 +23,22 @@ def user_login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form': form })
+    return render(request, 'account/login.html', {'form': form})
 
 
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})

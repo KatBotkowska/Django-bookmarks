8728cf0ca560045e9ca06d5a91ab7983ec455d22
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-01-04 21:02:06 +0100

    User profile, edit profile

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static #static not for production!!!
 from django.urls import include, path
 
 urlpatterns = [
@@ -21,3 +23,6 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
